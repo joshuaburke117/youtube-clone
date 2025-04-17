3b435d2b199007d9fb197f17a1c1b7d34d578753
joshuaburke117 <76673057+joshuaburke117@users.noreply.github.com>
2025-04-17 22:33:46 +1000

    Lession 18: Add get videos Function - Complete

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -9,8 +9,18 @@ initializeApp();
 
 const storage = new Storage();
 const firestore = new Firestore();
-
 const rawVideoBucketName = "jb117-yt-raw-videos";
+
+const videoCollectionId = "videos";
+
+export interface Video {
+    id?:string,
+    uid?:string,
+    filename?:string,
+    status?: "processing"| "processed",
+    title?: string,
+    description?: string
+}
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -48,4 +58,10 @@ export const generateUploadUrl = onCall({maxInstances: 1}, async (request)=>{
 
 
   return {url, fileName};
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () =>{
+  const snapshot = await
+    firestore.collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc)=> doc.data());
 });
